946122e7404bce82c927b3551c8557d7c8e44c33
Greg Lim <limjunqu@gmail.com>
2018-02-27 16:48:38 +0800

    Update login.component.ts

--- app/login.component.ts
+++ app/login.component.ts
@@ -32,38 +32,3 @@ var result = this._loginService.login(this.form.controls['username'].value,
         }        
     }
 }
-
-/*
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-import { PasswordValidator } from './passwordValidator';
-import { LoginService } from './login.service';
-
-@Component({
-    selector:'login',
-    templateUrl: 'login.component.html'
-})
-export class LoginComponent  {
-
-    form: FormGroup;        
-
-    constructor(fb: FormBuilder, private _loginService: LoginService){        
-
-        this.form = fb.group({
-            username:['',Validators.required ],
-            password:['',Validators.compose([Validators.required, PasswordValidator.cannotContainSpace])]            
-        })
-    }
-
-    login(){        
-        var result = this._loginService.login(this.form.controls['username'].value,this.form.controls['password'].value); 
-
-        if(!result){
-            this.form.controls['password'].setErrors({
-                invalidLogin: true 
-            });
-        }        
-    }
-}
-*/
